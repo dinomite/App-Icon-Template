a4a03e7e497d7e85fbdead1f11104170c49b3b9b
Drew Stephens <drew@dinomite.net>
2016-09-26 17:15:12 -0400

    Add 10pt and iPad Pro sizes

--- resize.sh
+++ resize.sh
@@ -11,6 +11,11 @@ fi
 BASE=`basename "$1" .svg`
 SVG="$1"
 
+# iPhone Notification iOS 7-10
+$INK -z -D -e "$BASE-20.png" -f 	$SVG -w 20 -h 20 
+$INK -z -D -e "$BASE-20@2x.png" -f 	$SVG -w 40 -h 40 
+$INK -z -D -e "$BASE-20@3x.png" -f  $SVG -w 60 -h 60
+
 # iPhone Spotlight iOS5,6 Settings iOS and iPad 5-8 29pt
 $INK -z -D -e "$BASE-29.png" -f 	$SVG -w 29 -h 29
 $INK -z -D -e "$BASE-29@2x.png" -f 	$SVG -w 58 -h 58
@@ -46,6 +51,9 @@ $INK -z -D -e "$BASE-76@2x.png" -f 	$SVG -w 152 -h 152
 #iTunes Artwork
 $INK -z -D -e "$BASE-512.png" -f 	$SVG -w 512 -h 512
 $INK -z -D -e "$BASE-1024.png" -f 	$SVG -w 1024 -h 1024
+
+# iPad Pro App iOS 9-10 83.5pt
+$INK -z -D -e "$BASE-83.5@2x.png" -f 	$SVG -w 167 -h 167
 
 cp "$BASE-512.png" iTunesArtwork.png
 cp "$BASE-1024.png" iTunesArtwork@2x.png
